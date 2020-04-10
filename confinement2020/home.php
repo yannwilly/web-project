84209a60b12fb5566c44a6f0133ed061855225b0
yannwilly <44833025+yannwilly@users.noreply.github.com>
2020-04-10 22:38:21 +0530

    Add files via upload

---
+++ confinement2020/home.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>E commerce</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once('products.php') ?>
+<style>
+    body{
+        background-image: linear-gradient(to right, #92fe9d 0%, #00c9ff 100%);
+        background-repeat: false;
+    }
+</style>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+        <div class="container">
+            <a class="navbar-brand" href="#">Ecommerce</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+<!--starting point of row -->
+    <?php  for ($i=0; $i <sizeof($voiture); $i++){ ?>
+
+
+            <div class="col-md-3 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="<?php echo $voiture[$i]['imagePath']?>"  width="700" height="200" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                        <i class="fa fa-car" aria-hidden="true"></i>   <a href="#"><?php echo $voiture[$i]['name']?></a>
+                        </h4>
+                        <h5 class="alert alert-danger"><?php echo $voiture[$i]['price']?> $</h5>
+                        <p class="card-text"><?php echo $voiture[$i]['description']?></p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+
+    <?php } ?>
+        <!--end of row-->
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ confinement2020/products.php
@@ -0,0 +1,13 @@
+
+<?php 
+    $voiture= array(
+        array('imagePath'=>'https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500','name'=>'Mercedess AMG','price'=>'50000','description'=>'MErcedess car for sale'),
+        array('imagePath'=>'https://img.etimg.com/thumb/width-640,height-480,imgsize-84442,resizemode-1,msid-68271698/mahindra-group-firm-automobili-pininfarina-unveils-electric-super-car.jpg','name'=>'Mahindra car','price'=>'2000','description'=>'Mahindra electric car'),
+        array('imagePath'=>'https://specials-images.forbesimg.com/imageserve/5bad46204bbe6f1f844e9b98/960x0.jpg?cropX1=0&cropX2=2804&cropY1=564&cropY2=2142','name'=>'Audi Car','price'=>'6000','description'=>'Audi electric car'),
+        array('imagePath'=>'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201906/Hector_Exteriors_1.png?R7b64Mextdd83mYq0mgEkPYN40.wC4xh','name'=>'MG car','price'=>'4000','description'=>'MG Hector Car')
+    );
+
+
+
+
+?>
